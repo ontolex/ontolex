320b13cc50ad2637a17bc5dc8c1ac1703d1e6299
John McCrae <john@mccr.ae>
2015-01-16 17:28:17 +0100

    Rework many examples after going through spec

--- Examples/create_images.sh
+++ Examples/create_images.sh
@@ -5,5 +5,8 @@ do
     NAME=${f/%.ttl/}
     NAME=${NAME:2}
     echo $NAME
-    rapper -i turtle -o dot -I "http://www.w3.org/ns/lemon/$NAME#" $f | dot -Tpng > ${f/%ttl/png}
+    if [[ $NAME != lime-old* ]]
+    then
+        rapper -i turtle -o dot -I "http://www.w3.org/ns/lemon/$NAME#" $f | dot -Tpng > ${f/%ttl/png}
+    fi
 done
