14a9796a538fb9249859624405ae7cca32451194
John P. McCrae <john@mccr.ae>
2016-03-14 16:23:04 +0000

    Update and rotate images

--- Examples/create_images.sh
+++ Examples/create_images.sh
@@ -6,10 +6,15 @@ do
     NAME=${NAME:2}
     if [[ $NAME != lime-old* ]]
     then
-        if [[ $f -nt ${f/%ttl/png} ]]
+        if [[ $f -nt ${f/%ttl/png} || (-e ${f/%ttl/LR} && ${f/%ttl/LR} -nt ${f/%ttl/png}) ]]
         then
             echo $NAME
-            python3 convert_to_dot.py $f | dot -Tpng > ${f/%ttl/png}
+            if [ -e ${f/%ttl/LR} ]
+            then
+                python3 convert_to_dot.py $f | dot -Tpng -Grankdir=LR > ${f/%ttl/png}
+            else
+                python3 convert_to_dot.py $f | dot -Tpng > ${f/%ttl/png}
+            fi
         fi
     fi
 done
